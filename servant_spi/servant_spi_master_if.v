2a4128b87aba4d778799e0830bbc32cd022ef4c8
Kavish Ranawella <66011681+kavishranawella@users.noreply.github.com>
2024-12-10 02:36:24 -0500

    Fix errors in SPI master

--- servant_spi/servant_spi_master_if.v
+++ servant_spi/servant_spi_master_if.v
@@ -3,21 +3,21 @@ module servant_spi_master_if
   #(parameter ADDRESS_WIDTH = 24,
     parameter CLOCK_DIVIDER = 2)
    (// Wishbone Slave Interface
-    input                                      clock,
-    input                                      reset,
-    input [31:0]                               wr_data,
-    input [ADDRESS_WIDTH-1:2]                  address,
-    input [3:0]                                wb_sel,
-    input                                      wb_we,
-    input                                      wb_cyc,
-    output [31:0]                              rd_data,
+    input wire                                     clock,
+    input wire                                     reset,
+    input wire [31:0]                               wr_data,
+    input wire [ADDRESS_WIDTH-1:2]                  address,
+    input wire [3:0]                                wb_sel,
+    input wire                                     wb_we,
+    input wire                                     wb_cyc,
+    output wire [31:0]                              rd_data,
     output reg                                 wb_ack,
 
     // SPI Master Interface
-    input                                      spi_miso,
-    output                                     spi_sck,
+    input  wire                                    spi_miso,
+    output wire                                    spi_sck,
     output reg                                 spi_ss,
-    output                                     spi_mosi);
+    output wire                                    spi_mosi);
 
 // State encoding
 parameter  IDLE = 3'b000;
@@ -40,35 +40,38 @@ reg         [2:0]                               bit_cnt;
 reg         [2:0]                               state;
 reg                                             wr_cmd;
 reg         [ADDRESS_WIDTH-1:0]                 address_reg;
-reg         [7:0]                               wr_data_reg[4];
-reg         [7:0]                               rd_data_reg[4];
+reg         [31:0]                              wr_data_reg;
+reg         [31:0]                              rd_data_reg;
 reg         [1:0]                               byte_offset;
-reg         [1:0]                               num_bytes;
+reg         [1:0]                               last_byte;
 reg         [7:0]                               spi_out_reg;
 reg         [7:0]                               spi_in_reg;
 
-wire [1:0] sel_dec_offset;
-wire [2:0] sel_dec_num;
+wire [1:0] sel_dec_start;
+wire [1:0] sel_dec_last;
 
 assign spi_sck = serial_clk;
 assign spi_mosi = spi_out_reg[7];
-assign rd_data = {rd_data_reg[3], rd_data_reg[2], rd_data_reg[1], rd_data_reg[0]};
-assign sel_dec_offset = wb_sel[0] ? 2'd0 : wb_sel[1] ? 2'd1 : wb_sel[2] ? 2'd2 : wb_sel[3] ? 2'd3 : 2'd0;
-assign sel_dec_num = {1'b0, wb_sel[0]} + {1'b0, wb_sel[1]} + {1'b0, wb_sel[2]} + {1'b0, wb_sel[3]};
+assign rd_data = rd_data_reg;
+//assign rd_data = {rd_data_reg[3], rd_data_reg[2], rd_data_reg[1], rd_data_reg[0]}; //TODO: Check and remove this
+assign sel_dec_start = wb_sel[0] ? 2'd0 : wb_sel[1] ? 2'd1 : wb_sel[2] ? 2'd2 : wb_sel[3] ? 2'd3 : 2'd0;
+assign sel_dec_last  = wb_sel[3] ? 2'd0 : wb_sel[2] ? 2'd3 : wb_sel[1] ? 2'd2 : wb_sel[0] ? 2'd1 : 2'd0;
 
 // Clock divider to generate serial clock
 always @(posedge clock or posedge reset) begin
-   if (reset) begin
+	if (reset) begin
 		serial_clk <= 1;
-   end else if (!spi_ss) begin
-		if (clk_cnt % (CLOCK_DIVIDER/2) == 0) begin
+	end else if (!spi_ss) begin
+		if ((state == TRANSMIT_DATA || state == READ_DATA) && byte_offset == last_byte && clk_cnt == 0 && bit_cnt == 0) begin // TODO: Try to improve this
+			serial_clk <= 1;
+		end else if (clk_cnt % (CLOCK_DIVIDER/2) == 0) begin
 			serial_clk <= ~serial_clk;
 		end else begin
 			serial_clk <= serial_clk;
 		end
 	end else if (wb_cyc && !wb_ack) begin
 		serial_clk <= 0;
-	end else begin
+   	end else begin
 		serial_clk <= 1;
 	end
 end
@@ -88,7 +91,7 @@ always @(posedge clock or posedge reset) begin
 		bit_cnt <= 0;
    end else if ((spi_ss && !wb_cyc) || wb_ack) begin
 		bit_cnt <= 0;
-	end else if (clk_cnt == 0) begin
+	end else if (clk_cnt == 0 && (!spi_ss || wb_cyc)) begin
 		bit_cnt <= bit_cnt + 1;
 	end else begin
       bit_cnt <= bit_cnt;
@@ -126,7 +129,7 @@ always @(posedge clock or posedge reset) begin
 				end
 			end
 			TRANSMIT_DATA: begin
-				if (byte_offset == num_bytes) begin
+				if (byte_offset == last_byte) begin
 					state <= FINISH;
 				end
 				else begin
@@ -134,7 +137,7 @@ always @(posedge clock or posedge reset) begin
 				end
 			end
 			READ_DATA: begin
-			  if (byte_offset == num_bytes) begin
+			  if (byte_offset == last_byte) begin
 					state <= FINISH;
 			  end
 			  else begin
@@ -146,12 +149,16 @@ always @(posedge clock or posedge reset) begin
 			end
 		endcase
 	end else begin
-		state <= state;
+		if (state == FINISH) begin  //TODO: Try to improve
+			state <= IDLE;
+		end else begin
+			state <= state;
+		end
 	end
 end
 
 always @(negedge serial_clk) begin
-	if (bit_cnt == 0) begin
+	if (bit_cnt == 1) begin
 		case (state)
 			TRANSMIT_COMMAND: begin
 			  if (wb_we) begin
@@ -174,7 +181,7 @@ always @(negedge serial_clk) begin
 				spi_out_reg <= address_reg[7:0];
 			end
 			TRANSMIT_DATA: begin
-				spi_out_reg <= wr_data_reg[byte_offset];
+				spi_out_reg <= wr_data_reg[byte_offset*8+:8];
 			end
 			default: begin
 			end
@@ -208,7 +215,7 @@ always @(posedge serial_clk) begin
 	if (state == READ_DATA) begin
 		spi_in_reg <= {spi_in_reg[6:0], spi_miso};
 		if (bit_cnt == 0) begin
-			rd_data_reg[byte_offset] <= {spi_in_reg[6:0], spi_miso};
+			rd_data_reg[byte_offset*8+:8] <= {spi_in_reg[6:0], spi_miso};
 		end
 	end
 end
@@ -224,12 +231,13 @@ always @(state) begin
 		begin
         spi_ss = 0;
         wb_ack = 0;
-        wr_data_reg[3] = wr_data[31:24];
-        wr_data_reg[2] = wr_data[23:16];
-        wr_data_reg[1] = wr_data[15:8];
-        wr_data_reg[0] = wr_data[7:0];
-        address_reg    = {address, sel_dec_offset};
-		  num_bytes = sel_dec_num[1:0];
+		  wr_data_reg = wr_data;
+//        wr_data_reg[3] = wr_data[31:24];  //TODO: Check and remove this
+//        wr_data_reg[2] = wr_data[23:16];
+//        wr_data_reg[1] = wr_data[15:8];
+//        wr_data_reg[0] = wr_data[7:0];
+        address_reg    = {address, sel_dec_start};
+		  last_byte = sel_dec_last;
         if (wb_we) begin
             wr_cmd = 1'b1;
         end
